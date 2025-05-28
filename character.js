8af53ec0a1134f92153af27a052d76584fbf873f
eq <abigailmiddleton2008@gmail.com>
2025-05-28 19:27:53 -0400

    Update character.js

--- character.js
+++ character.js
@@ -1,4 +1,6 @@
 let currentPersonality = "lavi";
+let idleTimer;
+let hasReturnedDistracted = false;
 
 function initializePersonality() {
   const roll = Math.random();
@@ -7,7 +9,19 @@ function initializePersonality() {
   else if (roll < 0.50) currentPersonality = "maj";
   else currentPersonality = "lavi";
 
+  loadCharacterAssets();
+  setupIdleLogic();
+}
+
+function loadCharacterAssets() {
   document.getElementById("character-sprite").src = `assets/sprites/${currentPersonality}_idle.png`;
-  document.querySelector("#talk-button").style.backgroundImage = `url('assets/images/talk/${currentPersonality}_button.png')`;
-  document.querySelector("#speech-bubble").src = `assets/images/bubbles/${currentPersonality}_bubble.png`;
+  document.querySelector("#talk-button").style.backgroundImage = `url('assets/talk/${currentPersonality}_button.png')`;
+  document.querySelector("#speech-bubble").src = `assets/bubbles/${currentPersonality}_bubble.png`;
+}
+
+function swapPose(type) {
+  const sprite = document.getElementById("character-sprite");
+  sprite.src = `assets/sprites/${currentPersonality}_${type}.png`;
+  clearTimeout(idleTimer);
+  setupIdleLogic();
 }
